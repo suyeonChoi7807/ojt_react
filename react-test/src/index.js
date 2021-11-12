fa28bd04bd92dd11e142a97d7c133654ed2e44c0
csy <suyeon2205@naver.com>
2021-11-12 13:51:24 +0900

    21-11-12

--- react-test/src/index.js
+++ react-test/src/index.js
@@ -1,29 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter, NavLink, Route, Switch} from 'react-router-dom';
-//Link, NavLink, useParam
+import {BrowserRouter, Routes, Route, NavLink, useParams} from 'react-router-dom';
 
-function Home(){
-  return(
+function Home() {
+  return (
     <div>
-      <h2>HOME</h2>
+      <h2>Home</h2>
       Home...
     </div>
   );
 }
 
-function Topics(){
+const contents = [
+  { id:1, title:'HTML', description:'HTML is ...'},
+  { id:2, title:'JavaScript', description:'JavaScript is ...'},
+  { id:3, title:'React', description:'React is ...'}
+]
+
+function Topic() {
+  let params = useParams()
+  console.log('params', params);
+  let topic_id = params.topic_id;
+  console.log('params.topic_id', params.topic_id);
+  let selected_topic = {
+    title: 'Sorry',
+    description: 'Not Found'
+  };
+
+  for (let i = 0; i < contents.length; i++) {
+    if (contents[i].id === Number(topic_id)) {
+      selected_topic = contents[i];
+      break;
+    }
+  }
+
   return(
     <div>
-      <h2>Topics</h2>
-      Topics...
+      <h3>{selected_topic.title}</h3>
+      {selected_topic.description}
+      hi
     </div>
   );
 }
 
-function Contact(){
-  return(
+function Topics() {
+  let list = [];
+  for(let i = 0; i < contents.length; i++) {
+    list.push(<li key={contents[i].id}><NavLink to={"/topics/"+contents[i].id}>{contents[i].title}</NavLink></li>);
+  }
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        {list}
+      </ul>
+      <Routes>
+        <Route path=":topic_id" element={<Topic/>}> // /:변수 를 통해 해당 부분이 파라미터일 것을 암시
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
     <div>
       <h2>Contact</h2>
       Contact...
@@ -31,24 +72,22 @@ function Contact(){
   );
 }
 
-function App(){
+function App() {
   return (
     <div>
       <h1>React Router DOM 예제</h1>
       <ul>
-        <li><NavLink exact to="/">Home</NavLink></li>
+        <li><NavLink exact="true" to="/">Home</NavLink></li>
         <li><NavLink to="/topics">Topics</NavLink></li>
         <li><NavLink to="/contact">Contact</NavLink></li>
       </ul>
-      
-      <Switch>
-        <Route exact path="/"><Home></Home></Route>
-        <Route path="/topics"><Topics></Topics></Route>
-        <Route path="/contact"><Contact></Contact></Route>
-      </Switch>
-
+      <Routes>
+        <Route exact="true" path="/" element={<Home/>}/>
+        <Route path="/topics/*" element={<Topics/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
     </div>
   );
 }
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
